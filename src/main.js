55da3612320f6dd62dd7d1f9f8a640100d0e61e1
Tung Nguyen <tungng@augensoftwaregroup.com>
2018-11-15 13:53:58 +0700

    add whole admin page

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
+import './components'
+import './plugins'
+import { sync } from 'vuex-router-sync'
 
+import App from './App.vue'
+import router from '@/router'
+import store from '@/store'
+// Sync store with router
+sync(store, router)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
